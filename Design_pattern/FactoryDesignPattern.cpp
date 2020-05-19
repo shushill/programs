74204fdd6c6b6c2e70e6d0e84ed541c8bdcede0d
Sushil Verma <sushil.bit.cse@gmail.com>
2020-05-19 17:06:50 +0530

    adding design pattern

---
+++ Design_pattern/FactoryDesignPattern.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+enum PersonType{
+    PT_Student,
+    PT_Engineer,
+    PT_Doctor
+};
+class Person{
+public:
+    virtual void print() = 0;
+};
+class Student : public Person{
+public:
+    void print(){
+        cout << " Person is a Student"<<endl;;
+    }
+};
+class Engineer : public Person{
+public:
+    void print(){
+        cout << " Person is a Engineer"<<endl;
+    }
+};
+class Doctor : public Person{
+public:
+    void print(){
+        cout << " Person is a Doctor"<<endl;
+    }
+};
+class Factory{
+public:
+    Factory(){
+        cout << "Factory Class"<<endl;
+    }
+    static Person* create(PersonType type){
+        if(type == PT_Student) return new Student();
+        else if(type == PT_Engineer) return new Engineer();
+        else if(type == PT_Doctor) return new Doctor();
+        else return nullptr;
+    }
+};
+int main(){                       // client code
+    PersonType type = PT_Engineer;
+    Person *p = nullptr;
+    p = Factory::create(type);
+    p->print();
+    return 0;
+}
